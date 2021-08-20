0ac46afece6c058d438ddb767a9f08c8f3d2e823
newboys111 <newboys111@foxmail.com>
2021-08-20 16:59:02 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+ App({
+ 	onLaunch: function (options) {
+
+ 	 
+ 		this.globalData = {}
+ 	},
+ 	/*
+ 	onShow: function (options) {
+ 		// 启动，或者从后台进入前台
+ 		//GroupBiz.initGroupShareTicket(options);
+ 	},
+ 	onHide: function () {
+ 		// 小程序从前台进入后台
+ 		//GroupBiz.clearGroupShareTicket();
+ 	}*/
+ })
---
+++ cel/helper/ccmini_comm.js
@@ -0,0 +1,9 @@
+ /**
+ * Notes: 通用常量
+ * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY www.code942.com
+ * Date: 2020-11-14 07:48:00 
+ */
+
+module.exports = {
+ 
+}
